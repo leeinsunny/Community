c12d7552209cbe5d26eae664645f1630a01d30c4
leeinsunny <sera5278@naver.com>
2023-11-18 19:41:44 +0900

    first check to connect

---
+++ app/src/main/java/com/example/mysololife/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mysololife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //깃허브 확인용 주석임_20231118
